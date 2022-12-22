2c886e1097c121fe4df7174f5f7cee3f906d640c
William Desloge <william@mercijack.co>
2022-12-22 18:20:54 +0100

    feat(projects): plug to backend

--- src/components/ProjectsWithFilters.jsx
+++ src/components/ProjectsWithFilters.jsx
@@ -1,69 +1,63 @@
-import styles from '../styles/Projects.module.css';
+import { useProjects } from 'components/hooks/api/projects';
+
 import ProjectNode from './ProjectWithFiltersNode';
-import useSWR from 'swr';
-import { useState, useEffect, useMemo } from 'react';
+import styles from '../styles/Projects.module.css';
 
 // TODO:
 // 1. maxRecords 100 is enough?
 // 2. check if strict comparison is better ?
 
 function ProjectsWithFilters({ limit = 100, appliedFilters }) {
-  const fetcher = url => fetch(url).then(res => res.json());
-  const API = '/api/protected/projects';
-  const { data, error } = useSWR(API, fetcher);
-  const [projects, setProjects] = useState([]);
-  const [projectsFiltered, setProjectsFiltered] = useState([]);
+  const { data, isError, isLoading } = useProjects();
 
-  useMemo(() => {
-    if (data && !data.error) {
-      const projectsList = data.data
-        .slice(0, limit)
-        .map(project => <ProjectNode key={project.id} data={project.fields} />);
-      setProjects(projectsList);
-    }
-  }, [data, limit]);
+  if (isError) {
+    return <>An error has occurred. Contact us if the problem persists</>;
+  }
 
-  useEffect(() => {
-    const filteredProjectsArray = projects.filter(project => {
-      const { data } = project.props;
-      return Object.keys(appliedFilters).every(key => {
-        if (appliedFilters[key].length === 0) {
-          return true;
-        }
-        if (key === 'priceRange') {
-          const filterMin = +appliedFilters[key][0];
-          const filterMax = +appliedFilters[key][1];
-          const projectMin = +data[key][0];
-          const projectMax = +data[key][1];
-
-          const inRange = projectMin <= filterMax && projectMax >= filterMin;
-          return inRange;
-        }
-        return appliedFilters[key].some(value => {
-          if (Array.isArray(data[key])) {
-            return data[key].includes(value);
-          }
-          return data[key] == value;
-        });
-      });
-    });
-    setProjectsFiltered(filteredProjectsArray);
-  }, [projects, appliedFilters]);
-
-  if (error || (data && data.error)) return <p>An error has occurred. Contact us if the problem persists</p>;
-  if (!data)
+  if (isLoading && !data) {
     return (
       <h2>
         Loading <span className='sr-only'>all projects</span>...
       </h2>
     );
+  }
+  const projects = data?.slice(0, limit).map(project => <ProjectNode key={project.id} data={project.fields} />);
+  const filtered_projects = projects.filter(project => {
+    const { data } = project.props;
+
+    return Object.keys(appliedFilters).every(key => {
+      if (appliedFilters[key].length === 0) {
+        return true;
+      }
+      if (key === 'priceRange') {
+        const filterMin = +appliedFilters[key][0];
+        const filterMax = +appliedFilters[key][1];
+        const projectMin = +data[key][0];
+        const projectMax = +data[key][1];
+
+        const inRange = projectMin <= filterMax && projectMax >= filterMin;
+        return inRange;
+      }
+      return appliedFilters[key].some(value => {
+        if (Array.isArray(data[key])) {
+          return data[key].includes(value);
+        }
+        return data[key] == value;
+      });
+    });
+  });
 
   return (
     <>
       <h2>Available carbon credits</h2>
       <div className={styles['list-wrapper']}>
         <ul role='list' className='list'>
-          {projectsFiltered.length !== 0 ? projectsFiltered : <h4>No project correspond to applied filters</h4>}
+          { filtered_projects.length !== 0
+            ?
+            filtered_projects
+            :
+            <h4>No project correspond to applied filters</h4>
+          }
         </ul>
       </div>
     </>
